3a3df7f97fd4b31066bd390437bd082a9f9b2cb0
ParthPan7 <parthpanchal71196@gmail.com>
2022-04-15 23:51:15 -0300

    Added PopUp GUI Sample Menu

---
+++ PhysicsExploration/MenuGUI.cpp
@@ -0,0 +1,8 @@
+#include<afxwin.h>
+#include "MenuGUI.h"
+
+int MenuGUI::loadMenu()
+{
+	::MessageBox(NULL, _T("There would Simulation List here!"), _T("Physics Simulations"), MB_OK);
+	return 0;
+}
